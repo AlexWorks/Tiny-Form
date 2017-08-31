f004ce6d0923385c5e7682cc97431bf5355ba8fa
AlexWorks <mister.lexx.ua@gmail.com>
2017-08-31 19:48:14 +0300

    Set up a tiny form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
+
+  value;
+
+  handleSubmit(value) {
+    if( value.hasOwnProperty('firstname')) {
+      this.value = value;
+      console.log(value);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InputFieldsComponent } from './input-fields/input-fields.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputFieldsComponent
   ],
   imports: [
     BrowserModule
