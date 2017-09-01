714d4ad3a07bc5c86a3c81e1f3f78b2aa86cba37
AlexWorks <mister.lexx.ua@gmail.com>
2017-09-01 17:29:45 +0300

    Refactored using Form Builder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,45 @@
 import { Component } from '@angular/core';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss',
+    '../../node_modules/open-sans-fontface/open-sans.scss']
 })
+
 export class AppComponent {
-  title = 'app';
 
-  value;
+  myForm: any;
 
-  handleSubmit(value) {
-    if( value.hasOwnProperty('firstname')) {
-      this.value = value;
-      console.log(value);
-    }
+  constructor(fb: FormBuilder ) {
+    this.myForm = fb.group({
+      'firstname': ['', Validators.required],
+      'lastname': ['', Validators.required],
+      'password': ['', Validators.compose([
+        Validators.required, this.passwordValidator
+      ])],
+      'sub': ['', null]
+    });
   }
+
+  ngOnInit() {
+  }
+
+  passwordValidator(control: FormControl): {[s: string]: boolean} {
+    if(control.value.length < 8) {
+      return {invalidPass: true};
+    }
+
+  }
+
+  handleCheckboxClick(event) {
+    let target = event.target.closest('.input-fields__notifications');
+    target.classList.toggle('input-fields__notification_checked');
+  }
+
+  onSubmit(value){
+    console.log(value);
+  }
+
 }
